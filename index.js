2f03a6fd8a587439788b49347da42e59b1385db7
Yuri Silveira <ypereirars@gmail.com>
2016-04-23 19:29:27 -0300

    Server basic handler function

--- index.js
+++ index.js
@@ -13,3 +13,25 @@ var mosca       = require('mosca'),
       persistence: persistence
     },
     moscaServer = new mosca.Server(settings);
+
+moscaServer.on('ready', function () {
+  console.log('Mosca server is up and running on port ' + settings.port);
+});
+
+
+moscaServer.published = function(packet, client, cb) {
+  if (packet.topic.indexOf('echo') === 0) {
+    return cb();
+  }
+
+  var newPacket = {
+    topic: 'echo/' + packet.topic,
+    payload: packet.payload,
+    retain: packet.retain,
+    qos: packet.qos
+  };
+
+  console.log('newPacket', newPacket);
+
+  moscaServer.publish(newPacket, cb);
+}
